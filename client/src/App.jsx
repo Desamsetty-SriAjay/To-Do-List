caa7c3d5d10759ff237cac6606b299b2b83e8807
sri547ajay <sriajayajay19@gmail.com>
2025-07-16 10:45:42 +0530

    bug removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import TodoForm from './components/todoForm';
+import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import { getTodos } from './services/api'; 
 
